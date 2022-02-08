b998c9c7eb9036eda79aa8ca2537abebb9ce33e3
fayto <ananastasyakazantseva59911@gmail.com>
2022-02-08 15:19:13 +0300

    Did properly: divided the input into two arrays, added methods for sum and product

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,23 +8,35 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         String[] nums = scan.nextLine().split(" ");
         int s = nums.length/2;
-        int sum1 = 0;
-        int sum2 = 0;
-        int pr1 = 1;
-        int pr2 = 1;
+        int[] arr1 = new int[s];
+        int[] arr2 = new int[nums.length - s];
         for (int i = 0; i < nums.length; i ++) {
             if (i < s) {
-                sum1 = sum1 + Integer.parseInt(nums[i]);
-                pr1 = pr1 * Integer.parseInt(nums[i]);
+                arr1[i] = Integer.parseInt(nums[i]);
             }
             else {
-                sum2 = sum2 + Integer.parseInt(nums[i]);
-                pr2 = pr2 * Integer.parseInt(nums[i]);
+                arr2[i-s] = Integer.parseInt(nums[i]);
             }
         }
-        System.out.println("sum1: " + sum1);
-        System.out.println("sum2: " + sum2);
-        System.out.println("pr1: " + pr1);
-        System.out.println("pr2: " + pr2);
+        System.out.println(Arrays.toString(arr1));
+        System.out.println("Sum: " + Sum(arr1) + "; " + "Product: " + Product(arr1));
+        System.out.println(Arrays.toString(arr2));
+        System.out.println("Sum: " + Sum(arr2) + "; " + "Product: " + Product(arr2));
+
     }
+    public static int Sum(int[] arr) {
+        int a = 0;
+        for (int i: arr) {
+            a = a + i;
+        }
+        return a;
+    }
+    public static int Product(int[] arr) {
+        int a = 1;
+        for (int i:arr) {
+            a = a * i;
+        }
+        return a;
+    }
+
 }
